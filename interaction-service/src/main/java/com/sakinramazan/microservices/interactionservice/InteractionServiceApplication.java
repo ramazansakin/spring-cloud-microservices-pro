e104fc278bf73fbed3872a8d60f1c98cebe1f2eb
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-27 21:41:20 +0300

    interaction of item service base impl

---
+++ interaction-service/src/main/java/com/sakinramazan/microservices/interactionservice/InteractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sakinramazan.microservices.interactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionServiceApplication.class, args);
+	}
+
+}
