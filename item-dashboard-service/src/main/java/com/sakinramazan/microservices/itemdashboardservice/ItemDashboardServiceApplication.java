b08fa6738da65759c0f5dfda02439e5f961c7a12
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-27 21:03:39 +0300

    item-dashboard service to list and manage items

---
+++ item-dashboard-service/src/main/java/com/sakinramazan/microservices/itemdashboardservice/ItemDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sakinramazan.microservices.itemdashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDashboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDashboardServiceApplication.class, args);
+	}
+
+}
