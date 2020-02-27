844fd107eab2270b2af2001361a0ba85ffa20bb6
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-27 21:06:45 +0300

    Eureka server configs impl

--- eureka-service/src/main/java/com/sakinramazan/microservices/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/sakinramazan/microservices/eurekaservice/EurekaServiceApplication.java
@@ -2,12 +2,14 @@ package com.sakinramazan.microservices.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
 
 }
